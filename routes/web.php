683894c89a6fc65c0895995b70dc6a9e75700d85
Gustavo-Henrique01 <gusta.2019.henrique@gmail.com>
2025-01-19 18:25:25 -0400

    merge da branch funcionalidades

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+
+
+
+//Route::post('/usuarios', [UsuarioController::class, 'criarUsuario']);
+
